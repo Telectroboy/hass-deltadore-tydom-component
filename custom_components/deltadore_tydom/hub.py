db15d80ad1bb3a6f98d143400275df136dcbf438
Telectroboy <telectroboy@hotmail.com>
2024-03-28 11:18:27 +0100

    Update hub.py
    refresh data period to 20min

--- custom_components/deltadore_tydom/hub.py
+++ custom_components/deltadore_tydom/hub.py
@@ -311,4 +311,4 @@ class Hub:
         """Periodically refresh data for devices which don't do push."""
         while True:
             await self._tydom_client.poll_devices_data_5m()
-            await asyncio.sleep(1800)
+            await asyncio.sleep(1200)
